587f59399ed00550df220f982cdc69c34a19fc60
Reaksmey396 <khimreaksmey123@gmail.com>
2026-09-12 14:11:05 +0700

    fix code to deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/Inventory-Management-System
-/',
+  base: '/Inventory-Management-System/',
 })
